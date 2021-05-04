da4d1e7fa3b2177563bd290ebda202c4d4161d22
Mateus Pinho <mateusleitepinho@gmail.com>
2021-05-04 14:00:39 +0100

    ok melhor

--- proj.pl
+++ proj.pl
@@ -2,11 +2,9 @@
 
 :- [codigo_comum].
 
-%predicados meus
-
-combinacoes_soma_helper(N, Els, Soma, Perms) :-
+combinacoes_soma_helper(N, Els, Soma, Combs) :-
     combinacao(N, Els, Comb),
-    setof(Comb, sum_list(Comb, Soma), [Perms | _]).
+    setof(Comb, sum_list(Comb, Soma), [Combs | _]).
 
 combinacoes_soma(N, Els, Soma, Combs) :-
     findall(X, combinacoes_soma_helper(N, Els, Soma, X), Combs).
@@ -14,12 +12,33 @@ combinacoes_soma(N, Els, Soma, Combs) :-
 % sublist(SL, L, N) - SL é uma sublista de L de comprimento N
 sublist(SL, L, N) :- append([_, SL, _], L), length(SL, N).
 
+% 3.1.2
+permutacoes_soma_helper(N, Els, Soma, Perms) :-
+    combinacao(N, Els, X),
+    permutation(X, Y),
+    setof(Y, sum_list(Y, Soma), [Perms | _]).
+
 permutacoes_soma(N, Els, Soma, Perms) :-
-    permutation(Els, Perm),
-    findall(X, sublist(X, Perm, N), PermTemp),
-    
-    writeln(PermA).
-    
-    
-    
-    
+    findall(X, permutacoes_soma_helper(N, Els, Soma, X), Perms).
+
+% 3.1.3
+
+espaco_fila_horizontal(Fila, Esp, 'h') :-
+
+
+espaco_fila(Fila, Esp, H_V) :-
+    espaco_fila_horizontal(Fila, Esp).
+
+
+% 3.1.4
+% 3.1.5
+% 3.1.6
+% 3.1.7
+% 3.1.8
+% 3.1.9
+% 3.1.10
+% 3.1.11
+% 3.1.12
+% 3.1.13
+% 3.1.14
+% 3.1.15
